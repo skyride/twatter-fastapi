3f6ef8b66e224ac3bf2d4c41108fd8a6877be0ce
Adam Findlay <adam.findlay@labela.nl>
2021-01-18 19:58:40 +0000

    Read endpoints

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+items = {}
+
+
+@app.get("/items/")
+def list_items():
+    """
+    List all items.
+    """
+    return list(items.values())
+
+
+@app.get("/items/{pk}")
+def get_item(pk: int):
+    """
+    Get a single item.
+    """
+    if pk in items:
+        return items[pk]
+
+    return JSONResponse(
+        content={"message": "Not Found"},
+        status_code=status.HTTP_404_NOT_FOUND)
